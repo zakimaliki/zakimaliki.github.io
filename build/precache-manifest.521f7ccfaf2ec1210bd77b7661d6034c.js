84af703bae50afcefba26ef35566fa770e9c94b4
zaki maliki <malikizakitop@gmail.com>
2020-06-02 13:50:41 +0700

    baru

---
+++ build/precache-manifest.521f7ccfaf2ec1210bd77b7661d6034c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95dc21b3a92cfc62a133f15d27ce8e82",
+    "url": "/zakimaliki.github.io/index.html"
+  },
+  {
+    "revision": "205fcfc1e3b6878bfda6",
+    "url": "/zakimaliki.github.io/static/css/main.8b076ee8.chunk.css"
+  },
+  {
+    "revision": "0773f578276a72f8392c",
+    "url": "/zakimaliki.github.io/static/js/2.348ea742.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/zakimaliki.github.io/static/js/2.348ea742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "205fcfc1e3b6878bfda6",
+    "url": "/zakimaliki.github.io/static/js/main.c0817fde.chunk.js"
+  },
+  {
+    "revision": "5630fb2e7253684c543e",
+    "url": "/zakimaliki.github.io/static/js/runtime-main.f3fbb9f8.js"
+  },
+  {
+    "revision": "b997820129b3ab5d80f5abd43161bd61",
+    "url": "/zakimaliki.github.io/static/media/clients-01.b9978201.svg"
+  },
+  {
+    "revision": "e54005bf054d03d33bd1b34a89fed3e7",
+    "url": "/zakimaliki.github.io/static/media/clients-02.e54005bf.svg"
+  },
+  {
+    "revision": "bbaa54a04b69de04ee674773e09302b1",
+    "url": "/zakimaliki.github.io/static/media/clients-03.bbaa54a0.svg"
+  },
+  {
+    "revision": "cdf0e500e4741076b6ee2e9787b4b445",
+    "url": "/zakimaliki.github.io/static/media/clients-04.cdf0e500.svg"
+  },
+  {
+    "revision": "b913b15068e1b11eb206a101eda47921",
+    "url": "/zakimaliki.github.io/static/media/clients-05.b913b150.svg"
+  },
+  {
+    "revision": "6dbc36133e254d171e27c7165309c05c",
+    "url": "/zakimaliki.github.io/static/media/feature-tile-icon-01.6dbc3613.svg"
+  },
+  {
+    "revision": "a02bfd4b6b5aa8cd6dc156002599a75a",
+    "url": "/zakimaliki.github.io/static/media/feature-tile-icon-02.a02bfd4b.svg"
+  },
+  {
+    "revision": "91b1b862e40b185edf9082c364243790",
+    "url": "/zakimaliki.github.io/static/media/feature-tile-icon-03.91b1b862.svg"
+  },
+  {
+    "revision": "687180834e71818255e449ec6ed473c0",
+    "url": "/zakimaliki.github.io/static/media/hero-image.68718083.png"
+  },
+  {
+    "revision": "4c66333f28a69b611caa3ab78e437a0b",
+    "url": "/zakimaliki.github.io/static/media/illustration-element-01.4c66333f.svg"
+  },
+  {
+    "revision": "ad9b81a8361d1ca5d6b49b80dd00035e",
+    "url": "/zakimaliki.github.io/static/media/illustration-section-01.ad9b81a8.svg"
+  },
+  {
+    "revision": "f062941c87f1c6075df1bcb7f49d85e4",
+    "url": "/zakimaliki.github.io/static/media/logo.f062941c.svg"
+  }
+]);
