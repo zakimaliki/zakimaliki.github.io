92c95654ce15c142fcc9186018a5dd9038fa7242
zaki maliki <malikizakitop@gmail.com>
2020-06-02 12:23:51 +0700

    baru

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'assets/css/style.css';
+import Homepage from 'scenes/Homepage'
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage></Homepage>
+    </div>
+  );
+}
+
+
+export default App;
